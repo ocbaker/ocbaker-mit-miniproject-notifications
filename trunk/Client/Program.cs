a1a36870fac162bcb6ae4113f4aab180d7c6b2cd
bakerstreet56 <bakerstreet56@fb0812e8-655e-15dc-de42-108f42cd926b>
2011-09-06 23:11:28 +0000

    Making a change for change's sake

--- trunk/Client/Program.cs
+++ trunk/Client/Program.cs
@@ -16,6 +16,7 @@ namespace Client
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //A Change
         }
     }
 }
