45696111c8ea2b0041b39a9d2c65c7ea202ea42b
shane@itsasmurflife.com <shane@itsasmurflife.com@fb0812e8-655e-15dc-de42-108f42cd926b>
2011-09-08 00:44:44 +0000

    ExtensionServices, pgeSMS updated.

--- trunk/Extensions/ExtensionServices.cs
+++ trunk/Extensions/ExtensionServices.cs
@@ -12,40 +12,53 @@ namespace Extensions
     {
         #region System.Windows.Controls
         #region Textbox
-
+        private static Brush DefaultBorderBrush;
         public static void SetStatus(this TextBox me, String statusText, TextBoxStatuses status)  {
+            if (DefaultBorderBrush == null){
+                DefaultBorderBrush = me.BorderBrush;
 
+            }
             switch(status)
             {
                 case TextBoxStatuses.ERRORED: 
                     {
-                        me.BorderBrush = Brushes.Red;
-                        me.Background = Brushes.PaleVioletRed;
+                        me.BorderBrush = fromHex("#CD0A0A");
+                        me.Background = fromHex("#FEF1EC");
                         me.BorderThickness = new System.Windows.Thickness(1);
-
                         return;
                     }
                 case TextBoxStatuses.OK:
                     {
-                        me.BorderBrush = Brushes.DarkGreen;
-                        me.Background = Brushes.LightGreen;
-                        me.BorderThickness = new System.Windows.Thickness(2);
+                        me.BorderBrush = fromHex("#8CCE3B");
+                        me.Background = fromHex("#F1FBE5");
+                        me.BorderThickness = new System.Windows.Thickness(1);
                         return;
                     }
                 case TextBoxStatuses.DEFAULT:
                     {
-                        me.BorderBrush = Brushes.Gray;
+                        me.BorderBrush = DefaultBorderBrush;
                         me.Background = Brushes.White;
-                        me.BorderThickness = new System.Windows.Thickness(0);
+                        me.BorderThickness = new System.Windows.Thickness(1);
                         return;
                     }
-
             }
         #endregion
         #endregion
-            
+
             
         }
+        #region System.Windows.Media
+        #region Brushes
+
+            public static Brush fromHex(string hex) 
+            {
+                BrushConverter bc = new BrushConverter();
+                return (Brush)bc.ConvertFrom(hex);
+
+            }
+
+#endregion
+#endregion
     }
 
     public enum TextBoxStatuses
